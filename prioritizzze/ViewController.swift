752079366598887d346cf4b0d89f73253f4d60cc
Amir <mr.nuriev.amir@gmail.com>
2018-04-28 17:21:12 +0300

    Initial Commit

---
+++ prioritizzze/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  prioritizzze
+//
+//  Created by Амир Нуриев on 28.04.2018.
+//  Copyright © 2018 Amir Nuriev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
